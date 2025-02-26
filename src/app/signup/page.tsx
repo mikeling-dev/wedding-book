a91eb6e3feaa2d4f400053bb28d90eda3f4abe17
Mike Ling <mikeling.dev@gmail.com>
2025-02-26 16:04:49 +0800

    working on signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Signup() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-3xl font-bold">Signup page</h1>
     </main>
   );
 }
